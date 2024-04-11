bfe56f3304cbfc12d9fde66a6683fd3056eda8ce
samuelpariente <samuelipariente@gmail.com>
2024-04-11 11:17:29 +0200

    sam modif

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,129 +3,173 @@ from utils import *
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
 
 st.set_page_config(layout="wide")
 
 
-st.markdown("<h1 style='text-align: center;'>Covid-19: maps and graphs of the epidemic in France</h1>", unsafe_allow_html=True)
-
-st.markdown("<h6 style='text-align: center;'>By Sami Jallouli, Martin Lefèvre, Samuel Pariente, Guillaume Grasset-Gothon</h6>", unsafe_allow_html=True)
 
 
+def Overview_page():
+    st.markdown("<h1 style='text-align: center;'>Covid-19: maps and graphs of the epidemic in France</h1>", unsafe_allow_html=True)
 
-st.header('State of the epidemic in France')
+    st.markdown("<h6 style='text-align: center;'>By Sami Jallouli, Martin Lefèvre, Samuel Pariente, Guillaume Grasset-Gothon</h6>", unsafe_allow_html=True)
 
-df = pd.read_csv('./preprocessed_data/epidemic_state.csv')
-df['date'] = pd.to_datetime(df['date'])
 
-st.write(f'As of May 31, 2023, the total number of infections detected since the start of the epidemic has reached {"{:,}".format(np.sum(df["P"]))} cases')
 
-# Global figures
-col = st.columns((1.5, 1.5, 1.5), gap="medium")
-today = df["date"].max()
-st.markdown(
-    """
-<style>
-.big-font {
-    font-size:45px !important;
+
+    st.header('State of the epidemic in France')
+
+    df = pd.read_csv('./preprocessed_data/epidemic_state.csv')
+    df['date'] = pd.to_datetime(df['date'])
+
+    st.write(f'As of May 31, 2023, the total number of infections detected since the start of the epidemic has reached {"{:,}".format(np.sum(df["P"]))} cases')
+
+    # Global figures
+    col = st.columns((1.5, 1.5, 1.5), gap="medium")
+    today = df["date"].max()
+    st.markdown(
+        """
+    <style>
+    .big-font {
+        font-size:45px !important;
+    }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+
+    with col[0]:
+        with st.container():
+            st.subheader("Total number of Covid cases last week")
+            df_week = df[df["date"] > today - timedelta(days=7)]
+            st.markdown(f"""<p class="big-font">{np.sum(df_week['P'])}</p>""", unsafe_allow_html=True)
+
+    with col[1]:
+        with st.container():
+            st.subheader("Percentage of cases among last week's tests")
+            percentage_positive = np.sum(df_week["P"])/np.sum(df_week['T']) * 100
+            st.markdown(f"""<p class="big-font" ">{percentage_positive:.2f} %</p>""", unsafe_allow_html=True)
+
+    with col[2]:
+        with st.container():
+            st.subheader("Evolution of Covid cases")
+            df_previous_week = df[(df["date"] > today - timedelta(days=14)) & (df["date"] < today - timedelta(days=7))]
+            evolution = (np.sum(df_week["P"]) - np.sum(df_previous_week["P"]))/np.sum(df_week["P"]) * 100
+            if evolution > 0:
+                st.markdown(
+                    f"""<p class="big-font" style="color: #228B22;">{evolution:.2f} %</p>""",
+                    unsafe_allow_html=True,
+                )
+            else:
+                st.markdown(
+                    f"""<p class="big-font" style="color: #ff0000;">{evolution:.2f} %</p>""",
+                    unsafe_allow_html=True,
+                )
+
+
+    end_date = max(df['date'])
+    # Add 9 months to the start date
+    start_date =  pd.to_datetime(end_date) - pd.DateOffset(months=9)
+
+
+    min_value = datetime(2020, 5, 13)
+    max_value = datetime(2023, 6, 23) - timedelta(days=30*9)
+    start_value = datetime(2023, 6, 23) - timedelta(days=30*9)
+
+    st.subheader("Epidemic evolution in terms of number of cases")
+    st.write('Number of Covid-19 cases reported at the time of testing in hospitals and Ehpad.')
+
+    # Create a slider for date selection
+    selected_date = st.slider(
+    "Move the sliding window:",
+    min_value=min_value,
+    max_value=max_value,
+    value=start_value,
+    format="YYYY-MM-DD"
+    )
+    end_object = datetime.strptime(str(selected_date), '%Y-%m-%d %H:%M:%S')
+
+    start_date = str(pd.Timestamp(end_object.date()) )
+
+    end_date =  str(pd.to_datetime(start_date)+ pd.DateOffset(months=9))
+
+    col1, col2  = st.columns(2)
+    with col1:
+        st.plotly_chart(plot_positive_cases_with_zoom(df,  start_date, end_date),height=30, width=300)
+        st.write('Move or resize the period you wish to inspect ')
+
+    with col2:
+        st.plotly_chart(plot_positive_cases(df, start_date, end_date))
+
+
+    st.subheader("National dynamics of the epidemic")
+    st.write('These graphs show the average weekly incidence rate (per 100,000 inhabitants, i.e. ), the number of tests and their positivity rate on May 31, 2023.')
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader("Incidence rate")
+        st.markdown(
+                    f"""<h4 style="color: #c8738b;">{df.iloc[-1]['Ti']:.0f} ‰ on {today.strftime("%A, %B %d, %Y")}</h4>""",
+                    unsafe_allow_html=True,
+                )
+        st.plotly_chart(plot_incidence_rate(df), use_container_width=True)
+
+    with col2:
+        st.subheader("People tested")
+        st.markdown(
+                    f"""<h4 style="color: #c8738b;">{df.iloc[-1]['T']//1000}k on {today.strftime("%A, %B %d, %Y")}</h4>""",
+                    unsafe_allow_html=True,
+                )
+        st.plotly_chart(plot_tested(df), use_container_width=True)
+
+    with col3:
+        st.subheader("Positive Rate")
+        st.markdown(
+                    f"""<h4 style="color: #c8738b;">{df.iloc[-1]['Tp']:.1f} % on {today.strftime("%A, %B %d, %Y")}</h4>""",
+                    unsafe_allow_html=True,
+                )
+        st.plotly_chart(plot_positive_rate(df), use_container_width=True)
+
+def Departement_page():
+    # By department
+    departements = ['France'] + list(unique_departments())
+    # create a select box for the user to select a department
+    st.subheader("Epidemic evolution by department")
+    st.write("This part of the dashboard shows the evolution of the epidemic in France and its departments. The map shows the incidence rate in each department, while the graph shows the evolution of the number of cases in the selected department.")
+
+    col_dep, col_wave  = st.columns(2)
+    with col_dep:
+        selected_departement = st.selectbox('Select a department:', departements)
+
+    with col_wave:
+        wave = st.selectbox("Wave", [1, 2, 3])
+
+
+
+    #try : 
+    map = map_cov(wave=wave, dep_to_highlight=selected_departement)
+    #except ValueError:
+    #    map = map_cov(wave=1, dep_to_highlight=None)
+
+
+    # Usage remains the same
+    line = plot_timeserie_with_animation(dep=selected_departement)
+
+    # make the title of the dashboard 
+    st.plotly_chart(map, use_container_width=True)
+    st.plotly_chart(line, use_container_width=True)
+
+
+#Add different pages
+PAGES = {
+    "Overview": Overview_page,
+    "Covid by Departement": Departement_page,
 }
-</style>
-""",
-    unsafe_allow_html=True,
-)
 
-
-with col[0]:
-    with st.container():
-        st.subheader("Total number of Covid cases last week")
-        df_week = df[df["date"] > today - timedelta(days=7)]
-        st.markdown(f"""<p class="big-font">{np.sum(df_week['P'])}</p>""", unsafe_allow_html=True)
-
-with col[1]:
-    with st.container():
-        st.subheader("Percentage of cases among last week's tests")
-        percentage_positive = np.sum(df_week["P"])/np.sum(df_week['T']) * 100
-        st.markdown(f"""<p class="big-font" ">{percentage_positive:.2f} %</p>""", unsafe_allow_html=True)
-
-with col[2]:
-    with st.container():
-        st.subheader("Evolution of Covid cases")
-        df_previous_week = df[(df["date"] > today - timedelta(days=14)) & (df["date"] < today - timedelta(days=7))]
-        evolution = (np.sum(df_week["P"]) - np.sum(df_previous_week["P"]))/np.sum(df_week["P"]) * 100
-        if evolution > 0:
-            st.markdown(
-                f"""<p class="big-font" style="color: #228B22;">{evolution:.2f} %</p>""",
-                unsafe_allow_html=True,
-            )
-        else:
-            st.markdown(
-                f"""<p class="big-font" style="color: #ff0000;">{evolution:.2f} %</p>""",
-                unsafe_allow_html=True,
-            )
-
-
-end_date = max(df['date'])
-# Add 9 months to the start date
-start_date =  pd.to_datetime(end_date) - pd.DateOffset(months=9)
-
-
-min_value = datetime(2020, 5, 13)
-max_value = datetime(2023, 6, 23) - timedelta(days=30*9)
-start_value = datetime(2023, 6, 23) - timedelta(days=30*9)
-
-st.subheader("Epidemic evolution in terms of number of cases")
-st.write('Number of Covid-19 cases reported at the time of testing in hospitals and Ehpad.')
-
-# Create a slider for date selection
-selected_date = st.slider(
-"Move the sliding window:",
-min_value=min_value,
-max_value=max_value,
-value=start_value,
-format="YYYY-MM-DD"
-)
-end_object = datetime.strptime(str(selected_date), '%Y-%m-%d %H:%M:%S')
-
-start_date = str(pd.Timestamp(end_object.date()) )
-
-end_date =  str(pd.to_datetime(start_date)+ pd.DateOffset(months=9))
-
-col1, col2  = st.columns(2)
-with col1:
-    st.plotly_chart(plot_positive_cases_with_zoom(df,  start_date, end_date),height=30, width=300)
-    st.write('Move or resize the period you wish to inspect ')
-
-with col2:
-    st.plotly_chart(plot_positive_cases(df, start_date, end_date))
-
-
-st.subheader("National dynamics of the epidemic")
-st.write('These graphs show the average weekly incidence rate (per 100,000 inhabitants, i.e. ), the number of tests and their positivity rate on May 31, 2023.')
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.subheader("Incidence rate")
-    st.markdown(
-                f"""<h4 style="color: #c8738b;">{df.iloc[-1]['Ti']:.0f} ‰ on {today.strftime("%A, %B %d, %Y")}</h4>""",
-                unsafe_allow_html=True,
-            )
-    st.plotly_chart(plot_incidence_rate(df), use_container_width=True)
-
-with col2:
-    st.subheader("People tested")
-    st.markdown(
-                f"""<h4 style="color: #c8738b;">{df.iloc[-1]['T']//1000}k on {today.strftime("%A, %B %d, %Y")}</h4>""",
-                unsafe_allow_html=True,
-            )
-    st.plotly_chart(plot_tested(df), use_container_width=True)
-
-with col3:
-    st.subheader("Positive Rate")
-    st.markdown(
-                f"""<h4 style="color: #c8738b;">{df.iloc[-1]['Tp']:.1f} % on {today.strftime("%A, %B %d, %Y")}</h4>""",
-                unsafe_allow_html=True,
-            )
-    st.plotly_chart(plot_positive_rate(df), use_container_width=True)
-
-
-
+demo_name = st.sidebar.selectbox("Choose a page", PAGES.keys())
+PAGES[demo_name]()
--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ import seaborn as sns
 from datetime import datetime
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import geopandas as gpd
 
 def load_data(filename):
     data = pd.read_csv(filename, sep=';')
@@ -125,3 +127,204 @@ def plot_incidence_rate(df):
     )
     return fig
 
+def prop_covid(date, dep_df):
+    # Filter the DataFrame for the given date and date-1
+    date_df = dep_df[dep_df['date_de_passage'] == date]
+
+    previous_date_df = dep_df[dep_df['date_de_passage'] == date - pd.DateOffset(days=1)]
+
+    value_at_date = date_df['prop_covid'].values[0] if not date_df.empty else 0
+    try : 
+        value_at_date_before = previous_date_df['prop_covid'].values[0] if not previous_date_df.empty else 0
+    except:
+        value_at_date_before = 0
+    return value_at_date, value_at_date_before
+
+def unique_departments():
+    weekly_df = pd.read_csv("raw_data/sursaud-covid19-departement.csv", sep = ";")
+    weekly_df["dep"] = weekly_df["dep"].apply(lambda x: int(x) if isinstance(x, str) and x.isdigit() else x)
+    return weekly_df['dep'].unique()
+
+def plot_timeserie_with_animation(dep):
+    dep = int(dep) if isinstance(dep, str) and dep.isdigit() else dep
+    dep = None if dep == "France" else dep
+    weekly_df = pd.read_csv("raw_data/sursaud-covid19-departement.csv", sep = ";")
+    weekly_df["date_de_passage"] = pd.to_datetime(weekly_df["date_de_passage"])
+    weekly_df["dep"] = weekly_df["dep"].apply(lambda x: int(x) if isinstance(x, str) and x.isdigit() else x)
+
+    if dep is not None:
+        # Filter data based on department and dates
+        weekly_df = weekly_df[weekly_df['dep'] == dep]
+        dep_df = weekly_df[['date_de_passage', 'nbre_pass_corona',"nbre_pass_tot"]].groupby('date_de_passage').sum().reset_index()
+        dep_df['prop_covid'] = dep_df['nbre_pass_corona'] / dep_df['nbre_pass_tot']
+        #make a moving average on nbre_pass_corona
+        dep_df['nbre_pass_corona'] = dep_df['nbre_pass_corona'].rolling(window=7).mean()
+
+    else:
+        #group by date and sum the values
+        dep_df = weekly_df[['date_de_passage', 'nbre_pass_corona',"nbre_pass_tot"]].groupby('date_de_passage').sum().reset_index()
+        dep_df['prop_covid'] = dep_df['nbre_pass_corona'] / dep_df['nbre_pass_tot']
+        #make a moving average on nbre_pass_corona
+        dep_df['nbre_pass_corona'] = dep_df['nbre_pass_corona'].rolling(window=7).mean()
+
+    wave1_end_date = '2020-09-14'
+
+    wave2_end_date = '2020-11-02'
+        
+    wave3_end_date = '2021-02-01'
+    
+    max_val = dep_df['nbre_pass_corona'].max()
+    dates = sorted(dep_df['date_de_passage'].unique())
+    
+    # Create a subplot with 2 rows and 1 column
+    fig = make_subplots(rows=2, cols=3, 
+                        specs=[[{"type": "scatter", "colspan": 3}, None, None],
+                               [{"type": "indicator"}, {"type": "indicator"}, {"type": "indicator"}]],
+                        vertical_spacing=0.2, column_widths=[0.3, 0.4, 0.3])
+    
+
+    fig.add_trace(go.Scatter(x=dep_df['date_de_passage'], y=dep_df['nbre_pass_corona'],
+                                mode='lines+markers', name=f"Cases for department {dep}", line=dict(color='#c8738b')),
+                    row=1, col=1)
+    #add waves end dates to the plot 
+
+    fig.add_vline(x=wave1_end_date, line_dash="dash", line_color="green", row=1, col=1)
+    fig.add_vline(x=wave1_end_date, line_dash="dash", line_color="green", row=1, col=1)
+    fig.add_vline(x=wave2_end_date, line_dash="dash", line_color="green", row=1, col=1)
+    fig.add_vline(x=wave3_end_date, line_dash="dash", line_color="green", row=1, col=1)
+    
+    gauge={
+            'axis': {'range': [None, 0.3], 'tickwidth': 1, 'tickcolor': "darkblue"},
+            'bar': {'color': "darkred"},
+            'steps': [
+                {'range': [0, 0.06], 'color': 'lightgray'},
+                {'range': [0.06, 0.12], 'color': 'gray'},
+                {'range': [0.12, 0.18], 'color': 'darkgray'},
+                {'range': [0.18, 0.24], 'color': 'gray'},
+                {'range': [0.24, 3], 'color': 'lightgray'}
+            ],
+            'threshold': {
+                'line': {'color': "red", 'width': 4},
+                'thickness': 0.75,
+                'value': 0.25
+            }
+        }
+
+    # Add a placeholder trace for the proportion over max value, to be updated in frames
+    fig.add_trace(go.Indicator(mode="number+gauge", value=dep_df['prop_covid'][0], delta={'reference': 1, 'relative': True},
+                               title={"text": "<span style='font-size:0.8em'>Proportion of covid case vs visits</span>"},gauge=gauge),
+                  row=2, col=2)
+    
+
+    # Add frames for each date with the red dashed line and update the indicator
+    frames = []
+    for date in dates:
+        prop_at_date, prop_at_date_before = prop_covid(date, dep_df)
+
+        # Update the indicators for each frame
+        frame_data = [
+            go.Scatter(
+                x=dep_df['date_de_passage'],
+                y=dep_df['nbre_pass_corona'],
+                mode='lines+markers'
+            ),
+            go.Indicator(
+                mode="number+gauge+delta",
+                value=prop_at_date ,
+                delta={'reference': prop_at_date_before, 'relative': True}
+            ),
+            ]
+
+        # Add shapes to indicate the current date with a vertical line
+        frame_layout = go.Layout(shapes=[dict(
+            type="line",
+            x0=date,
+            y0=0,
+            x1=date,
+            y1=max_val,
+            line=dict(color="Red", width=2, dash="dash")
+        )])
+
+        frames.append(go.Frame(
+            data=frame_data,
+            layout=frame_layout,
+            name=str(date)
+        ))
+
+    fig.frames = frames
+    # Set up the slider and play button
+    fig.update_layout(
+        updatemenus=[{
+            "type": "buttons",
+            "showactive": False,
+            "buttons": [{
+                "label": "Play",
+                "method": "animate",
+                "args": [None, {"frame": {"duration": 500, "redraw": True}, "fromcurrent": True}]
+            }]
+        }],
+        sliders=[{
+            "steps": [{"method": "animate", "args": [[str(date)], {"frame": {"duration": 500, "redraw": True}, "mode": "immediate"}], "label": str(date)} for date in dates]
+        }]
+    )
+    if dep is None:
+        fig.update_layout(title="Covid cases for all departments",
+                        xaxis_title="Date", yaxis_title="Covid cases", height=800)
+    else:
+        fig.update_layout(title=f"Covid cases for department {dep}",
+                        xaxis_title="Date", yaxis_title="Covid cases", height=800)
+
+    return fig
+
+
+def map_cov(dep_to_highlight=None, wave = 1):
+    dep_to_highlight = None if dep_to_highlight == "France" else dep_to_highlight
+
+    if wave == 1 :
+        weekly_df = pd.read_csv("preprocessed_data/weekly_covid_cases_by_department_wave_1.csv")
+    
+    if wave == 2 :
+        weekly_df = pd.read_csv("preprocessed_data/weekly_covid_cases_by_department_wave_2.csv")
+    
+    if wave == 3 :
+        weekly_df = pd.read_csv("preprocessed_data/weekly_covid_cases_by_department_wave_3.csv")
+
+    weekly_df["date_de_passage"] = pd.to_datetime(weekly_df["date_de_passage"])
+    max_cumulative_value = weekly_df['cumulative_nbre_pass_corona'].max()
+    
+    geo_df = gpd.read_file('raw_data/departements.geojson')
+    # Convert 'code' column to int except for '2A' and '2B'
+    geo_df["code"] = geo_df["code"].apply(lambda x: int(x) if x.isdigit() else x)
+    
+    if dep_to_highlight == None:
+       
+        fig = px.choropleth_mapbox(weekly_df, geojson=geo_df, locations='dep',
+                                featureidkey="properties.code",
+                                color='cumulative_nbre_pass_corona',
+                                center={"lat": 46.2276, "lon": 2.2137},
+                                mapbox_style="carto-positron", zoom=3.5,
+                                animation_frame='date_de_passage',
+                                color_continuous_scale='PuRd',
+                                range_color=[0, max_cumulative_value])
+        fig.update_layout(title_text=f'Covid cases in France by department during the {wave} wave')
+
+    if dep_to_highlight is not None:
+        dep_to_highlight = int(dep_to_highlight)
+        #long lat of the center of highlighted department
+        center_lat = geo_df[geo_df['code'] == dep_to_highlight].geometry.centroid.y.values[0]
+        center_lon = geo_df[geo_df['code'] == dep_to_highlight].geometry.centroid.x.values[0] 
+        
+        weekly_df =  weekly_df[weekly_df['dep'] == str(dep_to_highlight)]
+        fig = px.choropleth_mapbox(weekly_df, geojson=geo_df, locations='dep',
+                                featureidkey="properties.code",
+                                color='cumulative_nbre_pass_corona',
+                                center={"lat": center_lat, "lon": center_lon},
+                                mapbox_style="carto-positron", zoom=7,
+                                animation_frame='date_de_passage',
+                                color_continuous_scale='PuRd',
+                                range_color=[0, max_cumulative_value])
+        # add department name to the map
+        dep_name = geo_df[geo_df['code'] == dep_to_highlight].nom.values[0]
+        fig.update_layout(title_text=f'Covid cases in {dep_name} department during the {wave} wave')
+    
+    return fig
